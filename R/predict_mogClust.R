0ed35a4b576a587f4551d81fee644ce0957e4037
wei zong <weizong0527@gmail.com>
2022-06-23 13:49:07 -0400

    rewrite documentation

--- R/predict_mogClust.R
+++ R/predict_mogClust.R
@@ -2,29 +2,24 @@
 #'
 #' @param G The matrix for new omics data. Rows are samples and columns are omic features.
 #' @param X The matrix for new confounding variables. Rows are samples and columns are confounding
-#' variables.
+#' variables. If NULL, only predicted subtype/cluster assignment probabilities and labels will
+#' be generated.
 #' @param Y The matrix for new multivariate outcomes. Rows are samples and columns are outcome
-#' variables.
+#' variables. If NULL, only predicted subtype/cluster assignment probabilities and labels will
+#' be generated.
 #' @param result The list output from function \code{fit_mogClust}.
 #'
 #' @return A list object
 #' \itemize{
 #'  \item{\code{pred_prob}}{predicted subtype/cluster assignment probabilities.}
 #'  \item{\code{pred_lb}}{predicted subtype/cluster assignment labels.}
-#'  \item{\code{pred_outcome}}{predicted outcome matrix.}
+#'  \item{\code{pred_outcome}}{predicted outcome matrix. Only generated if both X and Y are
+#'  provided.}
 #' }
 #' @export predict_mogClust
-predict_mogClust = function(X, G, Y,result){#data to predict
-  X=as.matrix(X)#fit seperate intercept as B
+predict_mogClust = function(G, X = NULL, Y = NULL,result){
   G=as.matrix(G)
-  Y=as.matrix(Y)
-
-  if(nrow(G) != nrow(X) | nrow(G) != nrow(Y)){
-    stop("Number of samples of G, X, Y should be matched!")
-  }
   n = nrow(G)
-  np = ncol(X)
-  nq = ncol(Y)
   NG = ncol(G)
 
   Sigma = result$Sigma
@@ -38,8 +33,22 @@ predict_mogClust = function(X, G, Y,result){#data to predict
   pai=sapply(1:K, function(k)
     exp(G %*% gamma[,k,drop=F])/rowSums(exp(G %*% gamma)))
   predict_lb = factor(apply(pai,1,which.max))
-  Yhat = pai%*%t(B) + X%*%C
-  return(list(pred_prob = pai,
-              pred_lb = predict_lb,
-              pred_outcome = Yhat))
+
+  res = list(pred_prob = pai,
+             pred_lb = predict_lb)
+
+  if(!(is.null(X) | is.null(Y))){
+    X=as.matrix(X)
+    Y=as.matrix(Y)
+    if(nrow(G) != nrow(X) | nrow(G) != nrow(Y)){
+      stop("Number of samples of G, X, Y should be matched!")
+    }
+    np = ncol(X)
+    nq = ncol(Y)
+    Yhat = pai%*%t(B) + X%*%C
+
+    res$pred_outcome = Yhat
+  }
+
+  return(res)
 }
